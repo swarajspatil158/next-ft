d9aee6d874e1337cb174170128694e7b163b7646
swarajspatil158 <swarajspatil158@gmail.com>
2025-05-17 09:21:15 +0530

    Update theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -32,6 +32,10 @@ const config: Config = {
   				DEFAULT: 'hsl(var(--muted))',
   				foreground: 'hsl(var(--muted-foreground))'
   			},
+			placeholder: {
+				DEFAULT: 'hsl(var(--placeholder))',
+				foreground: 'hsl(var(--placeholder-foreground))'
+			},
   			accent: {
   				DEFAULT: 'hsl(var(--accent))',
   				foreground: 'hsl(var(--accent-foreground))'
@@ -40,7 +44,11 @@ const config: Config = {
   				DEFAULT: 'hsl(var(--destructive))',
   				foreground: 'hsl(var(--destructive-foreground))'
   			},
-  			border: 'hsl(var(--border))',
+  			border: {
+				DEFAULT: 'hsl(var(--border))',
+				dark: 'hsl(var(--border-dark))',
+				darker: 'hsl(var(--border-darker))'
+			},
   			input: 'hsl(var(--input))',
   			ring: 'hsl(var(--ring))',
   			chart: {
@@ -51,14 +59,17 @@ const config: Config = {
   				'5': 'hsl(var(--chart-5))'
   			}
   		},
+		lineHeight: {
+			base: '1.375',
+		},
   		borderRadius: {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
   		},
-		fontFamily: {
+		  fontFamily: {
 			inter: ["var(--font-inter)", 'sans-serif'],
-		},
+		  },
   	}
   },
   plugins: [require("tailwindcss-animate")],
